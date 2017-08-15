a103b74e8d8798036456837f8c04e2928bc8d175
yeli-buonya <yeli.e.buonya@gmail.com>
2017-08-14 22:00:23 -0700

    update styles to use aphrodite

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { StyleSheet, css } from "aphrodite";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const styles = StyleSheet.create({
+  body: {
+    margin: 0,
+    padding: 0,
+    fontFamily: "sans-serif"
+  }
+});
+
+ReactDOM.render(
+  <App className={css(styles.body)} />,
+  document.getElementById("root")
+);
 registerServiceWorker();
